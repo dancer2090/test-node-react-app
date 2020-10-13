7eb0f74adcea4afdf320f28a9b5d234fb247199e
yuriy-arttime <yuriy3304@gmail.com>
2020-10-13 10:00:42 +0300

    add multistart comands and nodemon

--- api/app.js
+++ api/app.js
@@ -6,7 +6,6 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var userRouter = require('./routes/user');
 
 var app = express();
 
